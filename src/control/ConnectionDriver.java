d78d0244469e95baa8b08812b4f5988eb47d2393
fotoply <norberg.niels@gmail.com>
2016-03-22 17:40:27 +0100

    Started work on GUI and added a way to get the current connection.

--- src/control/ConnectionDriver.java
+++ src/control/ConnectionDriver.java
@@ -1,6 +1,7 @@
 package control;
 
 import java.io.*;
+import java.nio.channels.NotYetConnectedException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -13,6 +14,7 @@ public class ConnectionDriver {
     private String password;
 
     private static ConnectionDriver instance;
+    private static Connection connection = null;
 
     public static ConnectionDriver getInstance() {
         if (instance == null) {
@@ -25,15 +27,22 @@ public class ConnectionDriver {
     }
 
     public void connect() {
-        Connection c = null;
         try {
             Class.forName("org.postgresql.Driver");
-            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb", "postgres", "123");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb", "postgres", "123");
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName()+": "+e.getMessage());
             System.exit(0);
         }
         System.out.println("Opened database successfully");
+    }
+
+    public static Connection getConnection() throws NotActiveException {
+        if (connection == null) {
+            throw new NotActiveException("No database connection is established");
+        } else {
+            return connection;
+        }
     }
 }
