0693fddf49b0a00f96429df4f09b99e973e5372d
fotoply <norberg.niels@gmail.com>
2016-03-22 20:48:31 +0100

    Class for text based driver added.

---
+++ src/control/TextDriver.java
@@ -0,0 +1,12 @@
+package control;
+
+/**
+ * Created 3/22/16
+ *
+ * @author Niels Norberg
+ */
+public class TextDriver {
+    public static void main(String[] args) {
+        //TODO insert init code
+    }
+}
